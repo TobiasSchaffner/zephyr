a8cf364ac494e02d63c163e08add546d6f831e76
Tobias Schaffner <tobiasschaffner87@outlook.com>
2020-07-08 17:36:30 +0200

    Send device to low power state

--- soc/arm/nordic_nrf/nrf52/power.c
+++ soc/arm/nordic_nrf/nrf52/power.c
@@ -10,6 +10,13 @@
 #include <logging/log.h>
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 
+static void _issue_low_power_command(void)
+{
+	__WFE();
+	__SEV();
+	__WFE();
+}
+
 /* Invoke Low Power/System Off specific Tasks */
 void sys_set_power_state(enum power_states state)
 {
@@ -18,6 +25,14 @@ void sys_set_power_state(enum power_states state)
  #ifdef CONFIG_HAS_SYS_POWER_STATE_DEEP_SLEEP_1
 	case SYS_POWER_STATE_DEEP_SLEEP_1:
 		nrf_power_system_off(NRF_POWER);
+		break;
+ #endif
+#endif
+#ifdef CONFIG_SYS_POWER_SLEEP_STATES
+ #ifdef CONFIG_HAS_SYS_POWER_STATE_SLEEP_1
+	case SYS_POWER_STATE_SLEEP_1:
+		nrf_power_task_trigger(NRF_POWER, NRF_POWER_TASK_LOWPWR);
+		_issue_low_power_command();
 		break;
  #endif
 #endif
@@ -35,6 +50,13 @@ void _sys_pm_power_state_exit_post_ops(enum power_states state)
  #ifdef CONFIG_HAS_SYS_POWER_STATE_DEEP_SLEEP_1
 	case SYS_POWER_STATE_DEEP_SLEEP_1:
 		/* Nothing to do. */
+		break;
+ #endif
+#endif
+#ifdef CONFIG_SYS_POWER_SLEEP_STATES
+ #ifdef CONFIG_HAS_SYS_POWER_STATE_SLEEP_1
+	case SYS_POWER_STATE_SLEEP_1:
+		__set_BASEPRI(0);
 		break;
  #endif
 #endif
